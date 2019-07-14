0ab851b005340c54f0c9f3921cdfbff0ce22242a
K. Mizutani <miztani@rcnp.osaka-u.ac.jp>
2019-07-14 19:18:45 +0900

    Refactor

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,5 @@
 
 int main() {
   Manager manager;
-  // manager.MakeMultipoleRootFile("rt/out.root");
-  manager.F_web(2000);
   return 0;
 }
--- src/manager.h
+++ src/manager.h
@@ -15,13 +15,24 @@ class Manager {
   TComplex F3(double W, double costh);
   TComplex F4(double W, double costh);
 
+  Resonance resonance(int i) const { return resonances_[i]; }
+
+  // Followings are methods for debugging.
+
+  // Generates a root file which contains CGLN amplitudes.
+  //
+  // eg.
+  //   manager.F_web(2000);
+  // in main.cc
   void F_web(int W);
 
   // Generates a root file which contains 21*4 TGraph's.
   // Energy dependences of multipoles for each resonance.
+  //
+  // eg.
+  //   manager.MakeMultipoleRootFile("rt/out.root");
+  // in main.cc
   void MakeMultipoleRootFile(const char *filename);
-
-  Resonance resonance(int i) const { return resonances_[i]; }
 
   static const int kNumResonances = 21;
 
